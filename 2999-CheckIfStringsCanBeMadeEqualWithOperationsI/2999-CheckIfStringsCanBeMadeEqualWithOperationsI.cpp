bc9f1e7ae9f7e45f4f8bfe5388b05b6748b81cfc
Gurnoor Singh <149313918+gurnoors05@users.noreply.github.com>
2026-08-16 18:52:57 +0530

    Create 2999-CheckIfStringsCanBeMadeEqualWithOperationsI

---
+++ 2999-CheckIfStringsCanBeMadeEqualWithOperationsI/2999-CheckIfStringsCanBeMadeEqualWithOperationsI.cpp
@@ -0,0 +1,9 @@
+// Last updated: 8/16/2026, 6:52:57 PM
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        bool condition1=((s1[0]==s2[0] && s1[2]==s2[2]) || (s1[0]==s2[2] && s1[2]==s2[0]));
+        bool condition2=((s1[1]==s2[1] && s1[3]==s2[3]) || (s1[1]==s2[3] && s1[3]==s2[1]));
+        return condition1 && condition2;
+    }
+};
