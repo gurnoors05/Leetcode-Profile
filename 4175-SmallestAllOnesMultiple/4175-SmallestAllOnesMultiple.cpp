964f57f5d2777ed6c335836b8d1a80c01b87689b
Gurnoor Singh <149313918+gurnoors05@users.noreply.github.com>
2026-08-16 18:49:26 +0530

    Create 4175-SmallestAllOnesMultiple

---
+++ 4175-SmallestAllOnesMultiple/4175-SmallestAllOnesMultiple.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/16/2026, 6:49:27 PM
+class Solution {
+public:
+    int minAllOneMultiple(int k) {
+        if (k % 2 == 0 || k % 5 == 0) return -1;
+
+        int rem = 0;
+
+        for (int len = 1; len <= k; len++) {
+            rem = (rem * 10 + 1) % k;
+            if (rem == 0) {
+                return len;
+            }
+        }
+
+        return -1;
+    }
+};
