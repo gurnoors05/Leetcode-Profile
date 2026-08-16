4db569bff2375776425a056a9b8925b4d4706302
Gurnoor Singh <149313918+gurnoors05@users.noreply.github.com>
2026-08-16 19:02:15 +0530

    Create 1450-DeleteLeavesWithAGivenValue

---
+++ 1450-DeleteLeavesWithAGivenValue/1450-DeleteLeavesWithAGivenValue.cpp
@@ -0,0 +1,28 @@
+// Last updated: 8/16/2026, 7:02:16 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(!root) {
+            return NULL;
+        }
+
+        root->left  = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+
+        if(root->left == NULL && root->right == NULL && root->val == target)
+            return NULL;
+
+        return root;
+    }
+};
