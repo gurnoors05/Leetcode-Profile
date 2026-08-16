44bb8aa80c89e4b6edd51e140f532b615dd18c49
Gurnoor Singh <149313918+gurnoors05@users.noreply.github.com>
2026-08-16 19:04:17 +0530

    Create 1250-LongestCommonSubsequence

---
+++ 1250-LongestCommonSubsequence/1250-LongestCommonSubsequence.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/16/2026, 7:04:18 PM
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<m;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
