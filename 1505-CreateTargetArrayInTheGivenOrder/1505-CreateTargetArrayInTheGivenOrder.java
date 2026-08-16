908634e78ae2a4d2e120edbac08a63729e488b99
Gurnoor Singh <149313918+gurnoors05@users.noreply.github.com>
2026-08-16 19:01:29 +0530

    Create 1505-CreateTargetArrayInTheGivenOrder

---
+++ 1505-CreateTargetArrayInTheGivenOrder/1505-CreateTargetArrayInTheGivenOrder.java
@@ -0,0 +1,23 @@
+// Last updated: 8/16/2026, 7:01:30 PM
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int target[]=new int[nums.length];
+        Arrays.fill(target,-1);
+        for(int i=0;i<nums.length;i++){
+           if(target[index[i]]==-1){
+            target[index[i]]=nums[i];
+           }
+           else{
+            for(int j=nums.length-1;j>index[i];j--){
+                
+                target[j]=target[j-1];
+                
+            }
+            
+           }
+            target[index[i]]=nums[i];
+
+        }
+        return target;
+    }
+}
