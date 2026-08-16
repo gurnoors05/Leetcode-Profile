0c04c02833c183b7d50ae8707c3812cf46afff0b
Gurnoor Singh <149313918+gurnoors05@users.noreply.github.com>
2026-08-16 18:59:32 +0530

    Create 1723-MaximumNumberOfAchievableTransferRequests

---
+++ 1723-MaximumNumberOfAchievableTransferRequests/1723-MaximumNumberOfAchievableTransferRequests.cpp
@@ -0,0 +1,44 @@
+// Last updated: 8/16/2026, 6:59:32 PM
+class Solution {
+public:
+    int m;
+    int result = INT_MIN;
+    int maximumRequests(int n, vector<vector<int>>& requests) {
+        m = requests.size();
+        
+        vector<int> resultant(n, 0);
+        
+        solve(0, 0, n, resultant, requests);
+        
+        return result;
+    }
+    void solve(int idx, int count, int n, vector<int>& resultant, vector<vector<int>>& requests) {
+        if(idx == m) {
+            
+            int allZero = true;
+            for(int &x : resultant) {
+                if(x != 0) {
+                    allZero = false;
+                    break;
+                }
+            }
+            
+            if(allZero) {
+                result = max(result, count);
+            }
+            return;
+        }
+        
+        
+        int from = requests[idx][0];
+        int to   = requests[idx][1];
+        
+        resultant[from]--;
+        resultant[to]++;
+        solve(idx+1, count+1, n, resultant, requests);
+        
+        resultant[from]++;
+        resultant[to]--;
+        solve(idx+1, count, n, resultant, requests);
+    }
+};
