8d209c8a811444dfbb41bad8390d61608f4fe5de
Gurnoor Singh <149313918+gurnoors05@users.noreply.github.com>
2026-08-16 19:07:08 +0530

    Create 935-OrderlyQueue

---
+++ 935-OrderlyQueue/935-OrderlyQueue.cpp
@@ -0,0 +1,16 @@
+// Last updated: 8/16/2026, 7:07:09 PM
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k>1){
+            sort(begin(s),end(s));
+            return s;
+        }
+        string result=s;
+        for(int i=1;i<s.length();i++){
+            string temp=s.substr(i)+s.substr(0,i);
+            result=min(result,temp);
+        }
+        return result;
+    }
+};
