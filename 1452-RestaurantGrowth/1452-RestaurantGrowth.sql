73bc4d5e751e0aa5452518e423fd796f6cffe655
Gurnoor Singh <149313918+gurnoors05@users.noreply.github.com>
2026-08-16 19:02:11 +0530

    Create 1452-RestaurantGrowth

---
+++ 1452-RestaurantGrowth/1452-RestaurantGrowth.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/16/2026, 7:02:11 PM
+# Write your MySQL query statement below
+SELECT visited_on, amount, average_amount 
+FROM (SELECT DISTINCT visited_on, SUM(amount) OVER
+ (ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW) AS amount,
+  ROUND(SUM(amount) OVER (ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW)/7,2)
+   AS average_amount
+FROM Customer) as whole_totals
+WHERE DATEDIFF(visited_on, (SELECT MIN(visited_on) FROM Customer)) >= 6
